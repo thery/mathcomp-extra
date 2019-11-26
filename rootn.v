0e47f7dead5944079fb04e6a085809e8ca5d83e7
thery <Laurent.Thery@inria.fr>
2019-11-26 16:25:33 +0100

    Show

--- rootn.v
+++ rootn.v
@@ -16,7 +16,7 @@ Fixpoint seq_pol_add (s1 s2 : seq nat) :=
   if s1 is a :: s3 then
   if s2 is b :: s4 then a + b :: seq_pol_add s3 s4
   else s1 else s2.
-
+ 
 Fixpoint seq_pol_mul (s1 s2 : seq nat) :=
   if s1 is a :: s3 then 
      seq_pol_add [seq a * i | i <- s2] (seq_pol_mul s3 (0 :: s2))
@@ -55,7 +55,8 @@ Lemma seq_pol_addP s1 s2 x :
    seq_pol_eval s1 x + seq_pol_eval s2 x.
 Proof.
 elim: s1 s2 => [| a s1 IH] [| b s2] //=.
-by rewrite IH mulnDr !addnA (addnAC a).
+Show 1.
+by rewrite seq_pol_add_cons /= IH mulnDr !addnA (addnAC a).
 Qed.
 
 Lemma seq_pol_eval0 s x : all (eq_op^~ 0) s -> seq_pol_eval s x = 0.
