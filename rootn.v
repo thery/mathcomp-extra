64983285c5585054924083fa88f8fb02f3682128
thery <Laurent.Thery@inria.fr>
2019-12-21 10:10:02 +0100

    new root

--- rootn.v
+++ rootn.v
@@ -12,214 +12,59 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Fixpoint seq_pol_add (s1 s2 : seq nat) :=
-  if s1 is a :: s3 then
-  if s2 is b :: s4 then a + b :: seq_pol_add s3 s4
-  else s1 else s2.
- 
-Fixpoint seq_pol_mul (s1 s2 : seq nat) :=
-  if s1 is a :: s3 then 
-     seq_pol_add [seq a * i | i <- s2] (seq_pol_mul s3 (0 :: s2))
-  else [::].
+Fixpoint rootn_rec k r n :=
+  if r == 0 then 0 else
+  if n is n1.+1 then
+  let v := (rootn_rec k (r %/ (2 ^ k)) n1).*2 in
+  if v.+1 ^ k <= r then v.+1 else v
+  else 0.
 
-Fixpoint seq_pol_exp (s : seq nat) n := iter n (seq_pol_mul s) [:: 1].
+Definition rootn k n :=
+  if k == 0 then 0 else rootn_rec k n n.
 
-Definition seq_2Xp1_exp_n (n : nat) := seq_pol_exp [:: 1; 2] n.
-
-Fixpoint seq_pol_eval (s : seq nat) (x : nat) :=
-  if s is a :: s1 then a + x * seq_pol_eval s1 x else 0.
-
-Fixpoint pre_rootn l n (fuel i : nat) :=
-  if fuel is fuel1.+1 then
-    if i == 0 then 
-      (0, 0)
-    else
-      let i' := i %/ n in
-      let k' := i %% n in
-      let (r, k2) := pre_rootn l n fuel1 i' in
-        let remainder := seq_pol_eval l r in
-        if remainder <=  n * k2 + k' then
-           (r.*2.+1, n * k2 + k'- remainder)
-        else
-           (r.*2, n * k2 + k')
-  else (0, 0).
-
-Definition rootn (n i : nat) := 
-  (pre_rootn ((take n (seq_2Xp1_exp_n n))) (2 ^ n) i i).1.
-
-Definition is_rootn (n i : nat) :=
-  (pre_rootn ((take n (seq_2Xp1_exp_n n))) (2 ^ n) i i).2  == 0.
-
-Lemma seq_pol_addP s1 s2 x :
-   seq_pol_eval (seq_pol_add s1 s2) x = 
-   seq_pol_eval s1 x + seq_pol_eval s2 x.
+Lemma rootn_rec_bound k r n : 
+  0 < k -> r <= n -> (rootn_rec k r n) ^ k  <= r < (rootn_rec k r n ).+1 ^ k.
 Proof.
-elim: s1 s2 => [| a s1 IH [| b s2]] //=; first by rewrite addn0.
-by rewrite IH mulnDr !addnA (addnAC a).
+case: k => // k _.
+have F : 1 < 2 ^ k.+1 by rewrite -{1}[1](exp1n k.+1) ltn_exp2r.
+elim: n r => [[] //=|n IH [|[|r]] rLn /=].
+- by rewrite exp0n // exp1n.
+- by rewrite exp0n // exp1n.
+- rewrite divn_small //=.
+  have -> : rootn_rec k.+1 0 n = 0by case: (n).
+  by rewrite exp1n /= exp1n.
+set x := _ %/ _.
+set u := rootn_rec _ _ _.
+have /IH : x <= n.
+  apply: leq_trans (rLn : _ < n).
+  have /mulnK H : 0 < 2 ^ k.+1 by rewrite expn_gt0.
+  rewrite -[r.+1]H leq_div2r //.
+  by rewrite -{1}[r.+1]muln1 ltn_mul2l.
+rewrite -/u => /andP[uLx xLu].  
+case: (leqP (u.*2.+1 ^ k.+1 ) r.+2) => ->; rewrite !(andbT, andTb).
+  rewrite -doubleS -muln2 expnMn.
+  rewrite (divn_eq r.+2 (2 ^ k.+1)) -/x.
+  apply: leq_trans (_ : x.+1 * 2 ^ k.+1 <= _).
+    by rewrite mulSn addnC ltn_add2r // ltn_mod ?expn_gt0.
+  by rewrite leq_mul2r // expn_eq0.
+rewrite -muln2 expnMn (divn_eq r.+2 (2 ^ k.+1)) -/x.
+apply: leq_trans (leq_addr _ _).
+by rewrite leq_mul2r // expn_eq0.
 Qed.
 
-Lemma seq_pol_eval0 s x : all (eq_op^~ 0) s -> seq_pol_eval s x = 0.
-Proof. 
-by elim: s => [| a s IH] //= /andP[/eqP -> s0]; rewrite add0n IH // muln0.
-Qed.
-
-Lemma seq_pol_mul0r s s2 x : 
-  all (fun y => y == 0) s2 -> seq_pol_eval (seq_pol_mul s s2) x = 0.
-Proof.
-elim: s s2 => [ | a s IH] s2 //= s20; rewrite seq_pol_addP.
-suff /seq_pol_eval0-> : all (eq_op^~ 0) [seq a * i | i <- s2].
-  by rewrite add0n IH.
-elim: s2 s20 => [ | b s2 ih] //= => /andP[/eqP -> s20].
-by rewrite muln0 eqxx ih.
-Qed.
-
-Lemma seq_pol_scal s a x : 
-  seq_pol_eval [seq a * i | i <- s] x = a * seq_pol_eval s x.
-Proof.
-by elim: s => [|b s IH] //=; rewrite ?muln0 // mulnDr IH mulnCA.
-Qed.
-
-Lemma pol_mulP s1 s2 x :
-   seq_pol_eval (seq_pol_mul s1 s2) x = seq_pol_eval s1 x * seq_pol_eval s2 x.
-Proof.
-elim: s1 s2 => [| a s1 IH] //= s2.
-by rewrite seq_pol_addP seq_pol_scal mulnDl IH /= add0n mulnA (mulnC _ x).
-Qed.
-
-Lemma seq_2Xp1_exp_nP n x :
-  seq_pol_eval (seq_2Xp1_exp_n n) x = x.*2.+1 ^ n.
-Proof.
-rewrite /seq_2Xp1_exp_n.
-elim: n => [| n IH]; first by rewrite /= muln0 expn0.
-by rewrite expnS /seq_pol_exp iterS pol_mulP IH /= muln0 addn0 muln2.
-Qed.
-
-Lemma seq_pol_add_size s1 s2 : 
-  size (seq_pol_add s1 s2) = maxn (size s1) (size s2).
-Proof.
-elim: s1 s2 => [| a s1 IH] s2 //=; first by rewrite max0n.
-case: s2 => [| b s2]; first by rewrite maxn0.
-by rewrite /= maxnSS IH.
-Qed.
-
-Lemma seq_pol_mul_size a b s2 :
-  size (seq_pol_mul [:: a; b] s2) = (size s2).+1.
-Proof.
-rewrite /= muln0 seq_pol_add_size /= !size_map.
-by apply/maxn_idPr; rewrite leqnSn.
-Qed.
-
-Lemma seq_pol_exp_size a b n : 
-  size (seq_pol_exp [:: a; b] n) = n.+1.
-Proof.
-rewrite /seq_pol_exp; elim: n => [| n IH] //.
-by rewrite iterS seq_pol_mul_size IH.
-Qed.
-
-Lemma add_small_large_coef s1 s2 n d : size s1 <= n ->
-  nth d (seq_pol_add s1 s2) n = nth d s2 n.
-Proof.
-elim: s1 s2 n => [| a s1 IH] s2 n //= sz.
-case: s2 => [| b s2]; first by rewrite !nth_default.
-by case: n sz => [| n] //; rewrite ltnS /= => s1n; apply: IH.
-Qed.
-
-Lemma seq_pol_eval_cut s n x :
-   seq_pol_eval (take n s) x + x ^ n * seq_pol_eval (drop n s) x = 
-   seq_pol_eval s x.
-Proof.
-elim: s n => [| a s IH] //= n; first by rewrite add0n muln0.
-case: n => [| n]; first by rewrite /= expn0 mul1n add0n.
-by rewrite /= expnS -mulnA -addnA -mulnDr IH.
-Qed.
-
-Lemma drop_seq_pol_add s1 s2 n : 
-   drop n (seq_pol_add s1 s2) = seq_pol_add (drop n s1) (drop n s2).
-Proof.
-elim: s1 s2 n => [| a s1 IH] // [| b s2] //= [| n] //.
-by case: (drop n s1).
-Qed.
-
-Lemma seq_2Xp1_exp_n_lead a b n : 
-  drop n (seq_pol_exp [:: a; b] n) = [:: b ^ n].
-Proof.
-elim: n => [| n IH] //.
-have -> : seq_pol_exp [:: a; b] n.+1 = 
-          seq_pol_mul [:: a; b] (seq_pol_exp [:: a; b] n).
-  by rewrite /seq_pol_exp iterS.
-rewrite [seq_pol_mul _ _](_ : _ =
-           seq_pol_add [seq a * c | c <- seq_pol_exp [:: a; b] n]
-              [seq b * c | c <- 0 :: seq_pol_exp [:: a; b] n]); last first.
-  by rewrite /=; congr (seq_pol_add _ _).
-rewrite drop_seq_pol_add drop_oversize; last first.
-  by rewrite size_map seq_pol_exp_size.
-rewrite -[LHS]/(drop n [seq b * c | c <- seq_pol_exp [:: a; b] n]).
-by rewrite -map_drop IH /= expnS.
-Qed.
-
-Lemma test_pol_correct n r:
-  seq_pol_eval (take n (seq_2Xp1_exp_n n)) r + 2 ^ n * r ^ n = r.*2.+1 ^ n.
-Proof.
-rewrite -seq_2Xp1_exp_nP -(seq_pol_eval_cut (seq_2Xp1_exp_n n) n r).
-by congr (_ + _); rewrite seq_2Xp1_exp_n_lead /= muln0 addn0 mulnC.
-Qed.
-
-Lemma pre_rootnP fuel i n (l := take n (seq_2Xp1_exp_n n)) :
-  i <= fuel -> 0 < n ->
-  (pre_rootn l (2 ^ n) fuel i).1 ^ n + (pre_rootn l (2 ^ n) fuel i).2 = i /\
-  i < ((pre_rootn l (2 ^ n) fuel i).1.+1) ^ n.
-Proof.
-revert l.
-case: n => [// | n] l ilefuel _; set n':= n.+1; move: ilefuel.
-elim: fuel i => [| fuel IH] i //.
-  rewrite leqn0 => /eqP -> /=; rewrite expnS mul0n !add0n; split => //.
-  by rewrite expn_gt0.
-move=> fuelc.
-elim/abstract_context : (pre_rootn l (2 ^ n.+1) fuel.+1 i) => Q defQ.
-rewrite /=.
-case: ifP => [/eqP iis0 | in0].
-  by rewrite defQ /= iis0 expnS mul0n !add0n expn_gt0; split.
-set q := (i %/ _).
-case recq : (pre_rootn _ _ _) => [r k'].
-have t2le2p : 2 <= 2 ^ n.+1.
-  elim: (n) => [//| k ih]; rewrite expnS mul2n -addnn.
-  by apply/(leq_trans ih)/leq_addr.
-have divgt0 : 0 < 2 ^ n.+1.
-  by apply: leq_trans t2le2p.
-have iltfuel : q <= fuel.
-  rewrite /q.
-  have igt0 : 0 < i by rewrite lt0n in0.
-  have := (ltn_Pdiv t2le2p igt0) => fact.
-  by rewrite -ltnS; apply: leq_trans fuelc.
-have := IH q iltfuel; rewrite recq; cbn [fst snd] =>[] [req rlt].
-case: ifP => [below | above].
-  rewrite defQ; cbn [fst snd]; rewrite -test_pol_correct -/n'.
-  rewrite (addnAC (seq_pol_eval _ _)) subnKC; last by exact below.
-  rewrite (addnC (2 ^ n' * k')) -addnA -mulnDr (addnC k') req.
-  rewrite addnC mulnC -divn_eq; split; first by [].
-  rewrite -doubleS -mul2n expnMn.
-  move: rlt; rewrite -(leq_pmul2l divgt0); apply: leq_trans.
-  by rewrite mulnC; apply: (ltn_ceil i divgt0).
-move/negbT: above; rewrite -ltnNge=> above.
-rewrite defQ; cbn [fst snd].
-rewrite addnA -mul2n expnMn mul2n -mulnDr mulnC req -divn_eq.
-split; first by[].
-rewrite -test_pol_correct (divn_eq i (2 ^ n.+1)) -/q addnC -req -(addnC k').
-rewrite mulnDl addnA (mulnC (r ^ n')) ltn_add2r.
-by rewrite addnC mulnC.
-Qed.
+Lemma root0n i : rootn 0 i = 0.
+Proof. by rewrite /rootn eqxx. Qed.
 
 Lemma rootn_bound n i : 0 < n -> rootn n i ^ n <= i < (rootn n i).+1 ^ n.
-Proof.
-rewrite /rootn => n_gt0; have [H1 H2] := pre_rootnP (leqnn i) n_gt0.
-by rewrite -[X in (_ <= X) && _ ]H1 leq_addr.
-Qed.
+Proof. by case: n => // n  _; apply: rootn_rec_bound. Qed.
 
-Lemma is_rootnE n i : 0 < n -> is_rootn n i = (i == rootn n i ^ n).
-Proof.
-rewrite /rootn /is_rootn => n_gt0; have [H1 H2] := pre_rootnP (leqnn i) n_gt0.
-by rewrite -[X in _ = (X == _)]H1 -[X in _ = (_ == X)]addn0 eqn_add2l.
-Qed.
+Lemma rootn0 n : rootn n 0 = 0.
+Proof. by case: n. Qed.
+
+Definition is_rootn n i := (i == rootn n i ^ n).
+
+Lemma is_rootnE n i : is_rootn n i = (i == rootn n i ^ n).
+Proof. by []. Qed.
 
 Lemma leq_rootn n x y : 0 < n -> x <= y -> rootn n x <= rootn n y.
 Proof.
