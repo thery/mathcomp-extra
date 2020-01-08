63d83765377ac59ae624a406a088e73dffe6d3bb
charguer <arthur@chargueraud.org>
2020-01-08 17:19:09 +0100

    cleanup and simplify

--- bignum.ml
+++ bignum.ml
@@ -1,30 +1,78 @@
 (** Representation for bnat:
    - list of bits
    - lower bits are at the head of the list
-   - leading bit must be B1
+   - leading bit must be B1 for a non-zero number
 *)
 
 type bit = B0 | B1
 
 type bnat = bit list
 
-(** smart constructor **)
 
-let bcons b l =
-  match b, l with
+(*****************************************************************************)
+(** * Auxiliary functions *)
+
+(** Smart constructor, ensuring normalization of [B0] to [] *)
+
+let bcons b n = (* returns b+2*n *)
+  match b, n with
   | B0, [] -> []
-  | _ ->  b :: l
+  | _ -> b::n
 
-(** Conversion to (nonnegative) primitive integers *)
+(** Auxiliary functions for addition and subtraction *)
+
+let two_bits_repr n = (* requires n <= 3 *)
+  (* returns (b0,b1) such that n=2*b1+b0 *)
+  match n with
+  | [] -> (B0,B0)
+  | [B1] -> (B1,B0)
+  | [b0;b1] -> (b0,b1)
+  | _ -> assert false
+
+(** Conversion of a bit to a (nonnegative) primitive integers *)
 
 let bit_to_nat b =
   match b with
   | B0 -> 0
   | B1 -> 1
 
-(** Comparisons *)
+(** Conversion of a bnat to a (nonnegative) primitive integers *)
 
-let rec cmp n1 n2 = (* returns -1, 0 or 1, according to sign(n1-n2) *)
+let rec bnat_to_nat n =
+  match n with
+  | [] -> 0
+  | b::n' -> bit_to_nat b + 2 * (bnat_to_nat n')
+
+(** Conversion of a bit to a bnat *)
+
+let bit_to_bnat b =
+  match b with
+  | B0 -> []
+  | B1 -> [B1]
+
+(** Conversion of a bit its opposite bit *)
+
+let bit_flip b = (* returns 1-b *)
+  match b with
+  | B0 -> B1
+  | B1 -> B0
+
+
+(*****************************************************************************)
+(** * Constants and comparisons *)
+
+(** Constants *)
+
+let zero = []
+
+let one = [B1]
+
+let two = [B0;B1]
+
+(** Comparison algorithm,
+    returns -1, 0 or 1, according to sign(n1-n2)  *)
+
+let rec cmp n1 n2 =
   match n1, n2 with
   | [], [] -> 0
   | [], _ -> -1
@@ -36,6 +84,8 @@ let rec cmp n1 n2 = (* returns -1, 0 or 1, according to sign(n1-n2) *)
       else if b1 = B0 then -1
       else 1
 
+(** Basic comparisons *)
+
 let (^=) n1 n2 =
   cmp n1 n2 = 0
 
@@ -45,32 +95,7 @@ let (^<=) n1 n2 =
 let (^<) n1 n2 =
   cmp n1 n2 < 0
 
-let rec bnat_to_nat n =
-  match n with
-  | [] -> 0
-  | b::n' -> bit_to_nat b + 2 * (bnat_to_nat n')
-
-(** Conversion of a bit to a bnat, or to its opposite bit *)
-
-let bit_to_bnat b =
-  match b with
-  | B0 -> []
-  | B1 -> [B1]
-
-let bit_flip b =
-  match b with
-  | B0 -> B1
-  | B1 -> B0
-
-(** Constants *)
-
-let zero = []
-
-let one = [B1]
-
-let two = [B0;B1]
-
-(** Derived comparisons *)
+(** Symmetric comparisons *)
 
 let (^<>) n1 n2 =
   not (n1 ^= n2)
@@ -81,41 +106,31 @@ let (^>=) n1 n2 =
 let (^>) n1 n2 =
   n2 ^< n1
 
-(** Even, multiplication and division by 2*)
 
-let even n =
-  match n with
-  | [] -> true
-  | B0::n' -> true
-  | _ -> false
+(*****************************************************************************)
+(** * Addition and subtraction *)
+
+(** Multiplication by 2 *)
 
 let mul2 n =
   bcons B0 n
 
-let div2 n =
-  match n with
-  | [] -> []
-  | b::n' -> n'
+(** Successor *)
 
-(** Successor, addition *)
-
-let rec succ n =
+let rec succ n = (* returns n+1 *)
   match n with
   | [] -> [B1]
   | B0::n' -> B1::n'
   | B1::n' -> B0::(succ n')
 
-let add_bit b n =
+(** Addition of a bit to a bnat *)
+
+let add_bit b n = (* returns n+b *)
   if b = B0
     then n
     else succ n
 
-let two_bits_repr n = (* assumes n <= 3 *)
-  match n with
-  | [] -> (B0,B0)
-  | [B1] -> (B1,B0)
-  | [b0;b1] -> (b0,b1)
-  | _ -> assert false
+(** Addition *)
 
 let (^+) n1 n2 =
   let rec aux n1 n2 b = (* b is the carry bit *)
@@ -129,25 +144,29 @@ let (^+) n1 n2 =
     in
   aux n1 n2 B0
 
-(** Predecessor, subtraction *)
+(** Predecessor *)
 
-let rec pred n =
+let rec pred n = (* returns max(n-1,0) *)
   match n with
-  | [] -> [] (* negative goes to zero *)
-  | B1::[] -> [] (* 1-1 is zero *)
+  | [] -> []
+  | B1::[] -> []
   | B1::n' -> bcons B0 n'
   | B0::n' -> B1::(pred n')
 
-let sub_bit b n =
+(** Subtraction of a bit to a bnat *)
+
+let sub_bit b n = (* returns n-b *)
   if b = B0
     then n
     else pred n
 
-(* substraction WARNING only valid if n1 >= n2 *)
-let (^-) n1 n2 =
+(* Subtraction *)
+
+let (^-) n1 n2 = (* requires n1 >= n2 *)
   let rec aux n1 n2 b = (* b is the carry bit *)
     match n1, n2 with
-    | [], _ -> [] (* negative goes to zero *)
+    | [], [] -> if b = B0 then [] else assert false
+    | [], _ -> assert false
     | _, [] -> sub_bit b n1
     | b1::n1', b2::n2' ->
         (* b1-b2-b = b3-2b', thus 2+b1-b2-b = b3+2*(1-b') *)
@@ -157,6 +176,10 @@ let (^-) n1 n2 =
         bcons b3 (aux n1' n2' b')
     in
   aux n1 n2 B0
+
+
+(*****************************************************************************)
+(** * Multiplication and exponentiation *)
 
 (** Multiplication (naive) *)
 
@@ -174,30 +197,69 @@ let rec (^^) a n =
     then one
     else a ^* a ^^ (n ^- one)
 
-(** Coprime *)
 
-let rec coprime n1 n2 =
-   if n1 ^= zero then n2 ^= one else
-   if n2 ^= zero then n1 ^= one else
-   let c = cmp n1 n2 in
-   if c = 0 then
-      n1 ^= one
-   else if even n1 && even n2 then
-      coprime (div2 n1) (div2 n2)
-   else if even n1 then
-      coprime (div2 n1) n2
-   else if even n2 then
-      coprime n1 (div2 n2)
-   else if c > 0 then
-      coprime (n1 ^- n2) n2
-   else
-      coprime n1 (n2 ^- n1)
+(*****************************************************************************)
+(** * Division, modulo and co-primality *)
 
-(** Modulo WARNING we assume n2 > 0*)
+(** Even test *)
 
-let rec (^%) n1 n2 =
+let even n =
+  match n with
+  | [] -> true
+  | B0::n' -> true
+  | _ -> false
+
+(** Division by 2 *)
+
+let div2 n =
+  match n with
+  | [] -> []
+  | b::n' -> n'
+
+(** Modulo *)
+
+let rec (^%) n1 n2 = (* requires n2 > 0 *)
   match n1 with
   | [] -> zero
   | b::n1' ->
      let r = add_bit b (mul2 (n1' ^% n2)) in
      if n2 ^<= r then r ^- n2 else r
+
+(** Coprime *)
+
+let rec coprime n1 n2 =
+   let c = cmp n1 n2 in
+   if c < 0 then
+      coprime n2 n1 (* swap to ensure n1 >= n2 in the code below *)
+   else if n2 ^= zero then
+      n1 ^= one (* zero is only coprime with one *)
+   else if c = 0 then
+      n1 ^= one (* a non-zero number is coprime with itself only if it is one *)
+   else
+      let simpl n = (* half a number if it is even *)
+        if even n then div2 n else n in
+      if even n1 || even n2
+        then coprime (simpl n1) (simpl n2)
+        else coprime (n1 ^- n2) n2
+
+
+(* alternative 1:
+
+     match even n1, even n2 with
+     | true, true  -> coprime (div2 n1) (div2 n2)
+     | true, false -> coprime (div2 n1) n2
+     | false, true -> coprime n1 (div2 n2)
+     | false, false -> coprime (n1 ^- n2) n2
+*)
+
+(* alternative 2:
+
+   if even n1 && even n2 then
+      coprime (div2 n1) (div2 n2)
+   else if even n1 then
+      coprime (div2 n1) n2
+   else if even n2 then
+      coprime n1 (div2 n2)
+   else
+      coprime (n1 ^- n2) n2
+*)
