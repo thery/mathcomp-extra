5dec08831ec23493b764b2125c8039590f4dcf72
thery <Laurent.Thery@inria.fr>
2020-01-10 00:21:39 +0100

    comments

--- aks_arthur.ml
+++ aks_arthur.ml
@@ -33,11 +33,22 @@ let rec log2_ceil n =
   log2_floor (2 * n - 1)
 
 (* Question: in maths paper on AKS, I see mostly rounding below,
-   is it really needed to round up here? *)
+   is it really needed to round up here? 
+   answer: the formal proof mostly uses log2n because it gives the 
+   simple bound n <= 2 ^ (log2n n)
+   rather than having to use n < 2 ^ (log2_floor n + 1)
+   if needed I could change the formal proof to use log2_floor
+   *)
 let log2 n =
   log2_ceil n
 
-(* Is fast exponentiation really needed? for "int"? for polynomials? *)
+(* Is fast exponentiation really needed? for "int"? for polynomials? 
+  answer : I think that for int naive is enough, since pow is only
+  needed in power_free and we know in this case that the exponent is always
+  smaller than log2 n, for polynomials I am afraid it is needed as we do
+  (X + c) ^ n, if we are not doing with fast exponential we are not
+  P(log2 n) anymore 
+  *)
 let rec pow a n =
   if n = 0 then 1 else
   if n = 1 then a else
@@ -48,6 +59,7 @@ let rec pow a n =
       else b2 * a
 
 (* TEMPORARY Naive power, in case it is sufficient *)
+
 let rec pow_naive a n =
   if n = 0 then 1 else a * pow_naive a (n-1)
 
@@ -64,7 +76,9 @@ let root k n =
 let is_root k n =
   (n = pow (root k n) k)
 
-(* square root -- is there an optimized version for this one? maybe it's not needed?*)
+(* square root -- is there an optimized version for this one? 
+   maybe it's not needed?
+   answer : I think it is not needed*)
 let sqrt =
   root 2
 
@@ -82,7 +96,7 @@ let rec coprime m n =
     then (m = 1)
     else coprime n (m mod n)
 
-(* the order of n in Z_m *)
+(* the order of n in Z_m : find the smallest r such that n ^ r = 1 mod m *)
 let order_mod m n =
   let rec aux m n r i k =
     let r1 = (r * n) mod m in
@@ -93,7 +107,8 @@ let order_mod m n =
     then aux m n 1 1 m
     else 0
 
-(* totient function *)
+(* totient function : number of elements smaller than n that are coprime with n 
+*)
 let totient n =
   let rec aux acc d =
     if d < n then
@@ -130,8 +145,8 @@ let rec replace i v l =
   | x::t -> if i = 0 then v::t else x::(replace (i-1) v t)
 
 (*-----------------------------------*)
-(* Modular polynomials of degree [k-1], working modulo [n],
-   represented as lists of length [k],
+(* Modular polynomials of degree [k-1], working with coefficient modulo [n]
+   and modulo the polynomial (X ^ k - 1) represented as lists of length [k],
    with the coefficient of X^0 to the front, and X^(k-1) to the tail. *)
 
 (* null polynomial, returns [0:: ... ::0] *)
@@ -148,13 +163,18 @@ let pXn n k i =
   replace (i mod k) 1 (pzero k)
 
 (* multiplication by X, turns a list [t++x::nil] into [x::t]
-   (argument n is not technically needed) *)
+   (argument n is not technically needed) 
+   X * (\sum_(i < k) a_i X^ i)  modulo (X^k - 1) =
+        a_k-1 + (\sum_(1 <= i < k) a_(i - 1) X^ i)  
+*)
 let pmulX n k v =
   let (x,t) = unlast v in
   x::t
 
 (* Addition (argument k is not actually needed),
-   returns the pointwise addition of two lists *)
+   returns the pointwise addition of two lists 
+   (x1 + X * t1) + (x2 + X * t2) = (x1 + x2) + X * (t1 + t2)
+   *)
 let rec padd n k v1 v2 =
   match v1, v2 with
   | [], [] -> assert (k = 0); []
@@ -162,14 +182,21 @@ let rec padd n k v1 v2 =
   | _ -> assert false
 
 (* Multiplication by a scalar [a]
-   (argument k is not actually needed) *)
+   (argument k is not actually needed)
+   a * (x + X * t) = (a* x) + X * (a * t)
+*)
+  
 let rec pscale n k a v =
   match v with
   | [] -> assert (k = 0); []
   | x::t -> ((a * x) mod n)::(pscale n (k-1) a t)
 
 (* Multiplication of a polynomial [v1] with a polynomial [v2]
-   (both lists of length [k]). *)
+   (both lists of length [k]).
+   v1 * (x + X * t2) = x * v1 + X * (v1 * t2)   
+*)
+
+  
 let rec pmul n k v1 v2 =
   let rec aux k2 v2 = (* [k2] is not actually needed *)
     match v2 with
@@ -187,7 +214,10 @@ let rec ppow n k p v =
     let r2 = pmul n k r r in
     if (p mod 2 = 0) then r2 else pmul n k v r2
 
-(* Equality test (argument k is not actually needed) *)
+(* Equality test (argument k is not actually needed)
+   (x1 + X * t1) = (x2 + X * t2)  iff
+   (x1 = x2) and (t1 = t2)
+*)
 let rec peq n k v1 v2 =
   match v1, v2 with
   | [], [] -> assert (k = 0); true
@@ -198,7 +228,15 @@ let rec peq n k v1 v2 =
 (*-----------------------------------*)
 (* The AKS algorithm *)
 
-(* Phase 1 : n cannot be written as m ^ e with 2 <= e <= l *)
+(* NB : in order to avoid computing log2 n again and again, 
+  in most functions, there is a companion argument to n 
+  (the number we try to assert its primality) that is l
+  l is meant to be initially set to log2 n *)
+
+(* Phase 1 : n cannot be written as m ^ e with 2 <= e <= l
+   since 2 <= m, the largest possible value for e with n = m ^ e
+   is log2 n since n <= 2 ^ (log2 n)
+ *)
 let power_free n l =
   let rec aux e =
     (e <= 1) || (not (is_root e n) && aux (e-1)) in
@@ -208,12 +246,30 @@ let power_free n l =
 
 type aks_param_res = Nice of int | Bad | Good of int
 
+(* this is the recursive part of the search. 
+   we try to find an h (k <= h < k + c) such that it is greater of a (a <= h)
+   and its order in Z_n is also greater than a (a <= order_mod k n)
+   If we find such h, we return it (Good h)
+   If during the search, it occurs that the candidate h is in fact
+   a divisor of n, we return the witness that n is not prime (Nice h)
+   If we find no such h we return Bad *)
+
 let rec aks_param_search n a k c =
   if c < 1 then Bad
   else if (n mod k = 0) then Nice k
   else if (a <= k) && (a <= order_mod k n) then Good k
   else aks_param_search n a (k + 1) (c - 1)
 
+(* run the search with some carefully choosen values: 
+   - n is the number whose primality is checked 
+   - l is meant to have value log2 n
+   - the bound parameter of the search is set to  (log2 n) ^ 2 
+   the range for the search is 2 <= h < (log2 n) ^ 5 /2 + 2
+   with this setting the search will always be positive 
+  (Bad will never been returned) 
+  anyway when an h is returned (Good h or Nice h) it will always
+  be smaller than (log2 n) ^ 5) /2 + 1 *)
+  
 let aks_param n l =
   let a = l * l in
   let k = 2 in
@@ -221,12 +277,22 @@ let aks_param n l =
   if l <= 1 then Nice n else aks_param_search n a k c
 
 (* Phase 3 : introspection with modular polynomial *)
-(* TEMPORARY is the test [1 <= r] equivalent to [r < 1] and thus to [r = 0] ? *)
-(* TEMPORARY add a comment to help read the formula computed for [xncc] *)
+(*  n is the prime we want to check
+    k is the result of the search (so k <= (log2 n) ^ 5 /2 + 1)
+    l has value (log2 n)
+    we work with polynomials with coef in Z_n and modulo X^k - 1 (so their
+    degree is strictly less than k)
+    and we check that a consequence of Fermat's little theorem, i.e
+      (X + c) ^ n = X^n + c (modulo X^k - 1)
+    holds for 1 <= c <= sqrt (totient k) * log2 n
+    since (totient k) <= k, we are still polynomial in log2 n
+  this is the key mathemical part of the algorithm, only checking this little 
+  sample is enough to assert the primality
+*)
 
 let fpoly_intro_range n k l =
   let rec aux n k c r =
-    if r < 1 then true else
+    if r = 0 then true else
       let cc = pconst n k c in
       let x = pXn n k 1 in
       let xcc = padd n k x cc in
@@ -240,6 +306,8 @@ let fpoly_intro_range n k l =
 
 (* Main algo *)
 
+(* we check if n is power_free, find the parameter k and check
+   if the polynomial equalities hold *)
 let aks n =
   let l = log2 n in
      power_free n l
